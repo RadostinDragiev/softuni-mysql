7d7301555e2f3a1416a899106debf7b7b7a85f09
Radostin Dragiev <56969624+RadostinDragiev@users.noreply.github.com>
2021-01-13 23:09:25 +0200

    Data definision

---
+++ 01-data definitions- and datatypes/data-definisions-lab.sql
@@ -0,0 +1,44 @@
+---------- 01. Create Tables ----------
+
+CREATE TABLE `employees`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`first_name` VARCHAR(45) NOT NULL,
+`last_name` VARCHAR(45) NOT NULL
+);
+CREATE TABLE `categories`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(45) NOT NULL
+);
+CREATE TABLE `products`(
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR(45) NOT NULL,
+`category_id` INT
+);
+
+
+---------- 02. Insert Data in Tables ----------
+
+INSERT INTO `employees`(`id`, `first_name`, `last_name`)
+VALUES(1, 'Ivan', 'Petkov'),
+(2, 'Petko', 'Misho'),
+(3, 'Joro', 'Kiro');
+
+
+---------- 03. Alter Table ----------
+
+ALTER TABLE `employees`
+ADD `middle_name` DECIMAL;
+
+
+---------- 04. Adding Constraints ----------
+
+ALTER TABLE `products`
+ADD CONSTRAINT `fk_products_cateories`
+FOREIGN KEY (category_id)
+REFERENCES categories(id);
+
+
+---------- 05. Modifying Columns ----------
+
+ALTER TABLE `employees` 
+CHANGE COLUMN `middle_name` `middle_name` VARCHAR(100);
